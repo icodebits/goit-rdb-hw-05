79f1be0dee80e38ed96d69d8420b64f87880d67d
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-10-24 18:23:57 +0300

    Add p2.

--- queries.sql
+++ queries.sql
@@ -6,3 +6,15 @@ SELECT
      WHERE orders.id = order_details.order_id) AS customer_id
 FROM 
     order_details;
+
+-- p2 -- ********************************************************************
+SELECT 
+    * 
+FROM 
+    order_details
+WHERE 
+    order_id IN (
+        SELECT id 
+        FROM orders 
+        WHERE shipper_id = 3
+    );
